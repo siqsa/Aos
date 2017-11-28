492872205d73e6764a303a083a70f0e723210aab
gotaehyuk <gotaehyuk@gmail.com>
2017-11-28 14:04:11 +0900

    321

---
+++ app/src/main/java/com/example/amdin/aos/NewActivity.java
@@ -0,0 +1,18 @@
+package com.example.amdin.aos;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by amdin on 2017-11-28.
+ */
+
+public class NewActivity extends AppCompatActivity {
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    setContentView(R.layout.activity_new);
+}
+}
